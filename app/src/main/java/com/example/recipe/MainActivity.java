ba1fe28c2e4048e0e4df957aeceda19ff62dd5b2
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-09-13 15:00:09 +0300

    0

--- app/src/main/java/com/example/recipe/MainActivity.java
+++ app/src/main/java/com/example/recipe/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.recipe;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +26,34 @@ public class MainActivity extends AppCompatActivity {
         initData();
 
         recyclerView=findViewById(R.id.recyclerView);
-        myAdapter=new MyAdapter(getApplicationContext(),recipes);
+        myAdapter=new MyAdapter(getApplicationContext(),recipes,MyDialog());
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
 
 
+
+
+
     }
+
+    private AlertDialog MyDialog() {
+                        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                        View view1=getLayoutInflater().inflate(R.layout.details_layout,null);
+
+                builder.setView(view1);
+
+
+                TextView title=view1.findViewById(R.id.title);
+                Button cancle=view1.findViewById(R.id.cancel_button);
+
+                title.setText("Chips");
+                return builder.create();
+
+
+
+    }
+
 
     private void initData() {
         recipes.add(new MyModel("Chips", R.drawable.img));
--- app/src/main/java/com/example/recipe/MyAdapter.java
+++ app/src/main/java/com/example/recipe/MyAdapter.java
@@ -1,13 +1,19 @@
 package com.example.recipe;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -16,10 +22,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
 
     Context context;
     List<MyModel> recipes;
+    AlertDialog myDialog;
 
-    public MyAdapter(Context context, List<MyModel> recipes) {
+    public MyAdapter(Context context, List<MyModel> recipes, AlertDialog myDialog) {
         this.context = context;
         this.recipes = recipes;
+        this.myDialog = myDialog;
     }
 
     @NonNull
@@ -37,6 +45,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
         holder.titleView.setText(recipe.getTitle());
         holder.imageView.setImageResource(recipe.getImage());
 
+        holder.details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
+                myDialog.show();
+
+            }
+        });
+
     }
 
     @Override
@@ -47,10 +64,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
     public class MyHolder extends RecyclerView.ViewHolder {
         TextView titleView;
         ImageView imageView;
+        Button details;
         public MyHolder(@NonNull View itemView) {
             super(itemView);
             titleView=itemView.findViewById(R.id.title);
             imageView=itemView.findViewById(R.id.image);
+            details=itemView.findViewById(R.id.detailsBtn);
         }
     }
 }
